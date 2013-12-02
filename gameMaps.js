5e87f285e1d5d92d04e2f0fb50bfbc176395e595
j4y4r <lechje@gmail.com>
2013-12-02 01:08:17 +0100

    added Stage

--- gameMaps.js
+++ gameMaps.js
@@ -160,7 +160,7 @@ gameMaps = [
       blocks: 8,
       fields:[
         {name: "score", length: 3, format: "asIs", settings:{append:"00"},
-        {name: "stage", length: 1},
+        {name: "stage", length: 1, format: "asIs"},
         {name: "name", lenght 3, format "ascii"},
 	{name: "trash", lenght: 1},
       ]
